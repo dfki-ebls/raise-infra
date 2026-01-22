c732c51c20c9a480078656dbfeefd554c506c727
Mirko Lenz <mirko@mirkolenz.com>
2026-01-22 14:00:20 +0100

    fix: improve upgrade

--- nixos/networking.nix
+++ nixos/networking.nix
@@ -1,7 +1,4 @@
 { pkgs, ... }:
-let
-  cpu = pkgs.stdenv.hostPlatform.parsed.cpu.name;
-in
 {
   networking = {
     useNetworkd = true;
--- nixos/update.nix
+++ nixos/update.nix
@@ -1,10 +1,14 @@
 { ... }:
+let
+  cpu = pkgs.stdenv.hostPlatform.parsed.cpu.name;
+in
 {
   system.autoUpgrade = {
     enable = true;
-    flake = "github:dfki-ebls/raise-infra";
+    flake = "github:dfki-ebls/raise-infra#${cpu}";
     dates = "04:00";
     allowReboot = true;
+    runGarbageCollection = true;
     rebootWindow = {
       lower = "03:30";
       upper = "05:00";
