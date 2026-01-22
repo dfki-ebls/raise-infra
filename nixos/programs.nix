8a95b82f6a3f9f6420a4ad33aa06ba7703ef359f
Mirko Lenz <mirko@mirkolenz.com>
2026-01-22 13:44:16 +0100

    feat: add more programs

--- nixos/programs.nix
+++ nixos/programs.nix
@@ -4,5 +4,13 @@
     bash.enable = true;
     fish.enable = true;
     zsh.enable = true;
+    git.enable = true;
+    nix-ld.enable = true;
+    neovim = {
+      enable = true;
+      defaultEditor = true;
+      viAlias = true;
+      vimAlias = true;
+    };
   };
 }
