377cf1ce35b443106bdd7286ad0705d190ce6fcf
Mirko Lenz <mirko@mirkolenz.com>
2026-03-09 09:20:10 +0100

    chore(user): remove unneeded options

--- nixos/users.nix
+++ nixos/users.nix
@@ -4,25 +4,20 @@
     mutableUsers = false;
     users.mlenz = {
       description = "Mirko Lenz";
-      home = "/home/mlenz";
       shell = pkgs.fish;
       uid = 1000;
       group = "mlenz";
-      extraGroups = [
-        "wheel"
-        "users"
-      ];
+      extraGroups = [ "wheel" ];
       isNormalUser = true;
       # no password set, authentication only via rssh
       openssh.authorizedKeys.keys = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFT0P6ZLB5QOtEdpPHCF0frL3WJEQQGEpMf2r010gYH3 mlenz@mirkos-macbook"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFT0P6ZLB5QOtEdpPHCF0frL3WJEQQGEpMf2r010gYH3 mlenz@macbook"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPg/jZmSr0LiCm5FKAcF54UJXK8GNgDO4op0MiASNadb mlenz@iphone"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHTD8HTidTJM3RLmU+WW7tBlDz6L2x8zoHJhqzA6m3+B mlenz@1password"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIq4FI/+G9JoUDXlUoKEdMtVnhapUScSqGg34r+jLgax mlenz@shellfish"
       ];
     };
-    groups.mlenz = {
-      gid = 1000;
-    };
+    groups.mlenz.gid = 1000;
     users.quadlet = {
       description = "Quadlet User";
       isSystemUser = true;
@@ -42,8 +37,6 @@
         }
       ];
     };
-    groups.quadlet = {
-      gid = 500;
-    };
+    groups.quadlet.gid = 500;
   };
 }
