6e36c46cda6d6d64523dddf3d12bcde5260627cc
Mirko Lenz <mirko@mirkolenz.com>
2026-03-23 13:29:30 +0100

    chore: remove caddy-docker

--- nixos/caddy.nix
+++ nixos/caddy.nix
@@ -5,18 +5,11 @@
   ...
 }:
 let
-  inherit (config.virtualisation) quadlet;
   mkHost = domain: if config.custom.enableCertificates then domain else "http://${domain}";
   mkSubHost = prefix: mkHost "${prefix}.${config.custom.rootDomain}";
 in
 {
   _module.args.caddyHelpers = { inherit mkHost mkSubHost; };
-  assertions = [
-    {
-      assertion = !(config.services.caddy.enable && quadlet.containers.caddy.enable);
-      message = "Only one of services.caddy.enable and virtualisation.quadlet.containers.caddy.enable can be set.";
-    }
-  ];
 
   services.caddy = {
     enable = true;
@@ -33,39 +26,6 @@ in
           reverse_proxy ${config.services.dex.settings.web.http}
         '';
       };
-    };
-  };
-  systemd.tmpfiles.settings.caddy = lib.mkIf quadlet.containers.caddy.enable {
-    "${config.services.caddy.dataDir}/data".d = {
-      user = "50000";
-      group = "50000";
-    };
-    "${config.services.caddy.dataDir}/config".d = {
-      user = "50000";
-      group = "50000";
-    };
-  };
-  virtualisation.quadlet.containers.caddy = {
-    enable = false;
-    imageStream = pkgs.caddy-docker;
-    containerConfig = {
-      Volume = [
-        "${config.services.caddy.configFile}:/etc/caddy/Caddyfile:ro"
-        "${config.services.caddy.dataDir}/data:/data"
-        "${config.services.caddy.dataDir}/config:/config"
-      ];
-      Network = [
-        quadlet.networks.external.ref
-        quadlet.networks.internal.ref
-      ];
-      PublishPort = [
-        "80:80"
-        "443:443"
-        "443:443/udp"
-      ];
-      AddCapability = [ "NET_BIND_SERVICE" ];
-      SubUIDMap = "quadlet";
-      SubGIDMap = "quadlet";
     };
   };
 
