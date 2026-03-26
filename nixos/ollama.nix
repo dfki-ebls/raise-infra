29c6cf5d6566ae5ac02e93e6fb5e63eb77f9a2f7
Mirko Lenz <mirko@mirkolenz.com>
2026-03-26 14:06:43 +0100

    chore(ollama): update config

--- nixos/ollama.nix
+++ nixos/ollama.nix
@@ -11,17 +11,24 @@ lib.mkIf config.custom.enableNvidia {
     openFirewall = true;
     syncModels = true;
     loadModels = [
-      "gpt-oss:20b"
-      "qwen3.5:35b"
+      # "gpt-oss:20b"
+      "qwen3.5:0.8b"
+      # "qwen3.5:2b"
+      # "qwen3.5:4b"
+      "qwen3.5:9b"
+      "qwen3.5:27b"
+      # "qwen3.5:35b"
     ];
+    # ollama serve --help
     environmentVariables = {
-      OLLAMA_CONTEXT_LENGTH = "16384";
+      OLLAMA_CONTEXT_LENGTH = toString 64 * 1024;
       OLLAMA_FLASH_ATTENTION = "1";
       OLLAMA_KEEP_ALIVE = "60m";
       OLLAMA_KV_CACHE_TYPE = "q8_0";
-      OLLAMA_MAX_LOADED_MODELS = "1";
-      OLLAMA_MAX_QUEUE = "64";
-      OLLAMA_NUM_PARALLEL = "1";
+      OLLAMA_MAX_LOADED_MODELS = "3";
+      OLLAMA_MAX_QUEUE = "128";
+      OLLAMA_NO_CLOUD = "1";
+      OLLAMA_NUM_PARALLEL = "4";
     };
   };
 }
