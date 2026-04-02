58f5a3c727e7e6d811a99e6e2417d45a9f86401c
Mirko Lenz <mirko@mirkolenz.com>
2026-04-02 10:42:56 +0200

    chore(pkgs/caddy): update hash

--- pkgs/caddy-custom.nix
+++ pkgs/caddy-custom.nix
@@ -4,5 +4,5 @@ caddy.withPlugins {
     # https://github.com/fabriziosalmi/caddy-waf/releases
     "github.com/fabriziosalmi/caddy-waf@v0.3.0"
   ];
-  hash = "sha256-c+dTA8sGkeMiAUmpdMt9zEQ3WjlC4cpPCcnI1Iz9W7E=";
+  hash = "sha256-cBAfvD2hbvsv8STeoVkmETemYAdO/uMx/fT4bv5T7h4=";
 }
