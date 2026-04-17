81ebe8129ad9edd57033a2cd52951d2c098975e1
Mirko Lenz <mirko@mirkolenz.com>
2026-04-17 08:47:57 +0200

    fix(llmhop): enable

--- nixos/llmhop.nix
+++ nixos/llmhop.nix
@@ -1,18 +1,12 @@
 { lib, config, ... }:
 {
   services.llmhop = {
-    # enable = config.custom.vllm.enable;
-    enable = false;
+    enable = config.custom.vllm.enable;
     settings = {
       listen = "127.0.0.1:18000";
-      # authTokens = [ "\${file:auth_token}" ];
       models = lib.mapAttrs (_: model: {
         url = "http://127.0.0.1:${toString model.port}";
       }) (lib.filterAttrs (_: model: model.enable) config.custom.vllm.models);
     };
   };
-
-  # systemd.services.llmhop.serviceConfig = lib.mkIf config.services.llmhop.enable {
-  #   LoadCredential = [ "auth_token:/etc/llmhop/auth-token" ];
-  # };
 }
