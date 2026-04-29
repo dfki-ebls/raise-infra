0a39adea9c97bc46b095456b245e818123b74fc8
Mirko Lenz <mirko@mirkolenz.com>
2026-04-29 10:55:23 +0200

    chore(deps): update caddy waf

--- pkgs/caddy-custom.nix
+++ pkgs/caddy-custom.nix
@@ -2,7 +2,7 @@
 caddy.withPlugins {
   plugins = [
     # https://github.com/fabriziosalmi/caddy-waf/releases
-    "github.com/fabriziosalmi/caddy-waf@v0.3.2"
+    "github.com/fabriziosalmi/caddy-waf@v0.3.3"
   ];
-  hash = "sha256-67EyPwLFPA1bi2D6ke8tvf6yckjas02pd+O1GNoLLVw=";
+  hash = "sha256-wKG8kLd1cB4bQaY/0vWjdY3b/w8sJqpmdGsg23ZcEPY=";
 }
