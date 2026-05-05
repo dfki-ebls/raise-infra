65807ddaf132a904df6ed14d93a2c12f2de8b854
Mirko Lenz <mirko@mirkolenz.com>
2026-05-05 22:04:33 +0200

    fix(dfki): add allowed sources for hivegent

--- dfki/vpn.nix
+++ dfki/vpn.nix
@@ -8,4 +8,7 @@
   services.caddy.virtualHosts.dex = lib.mkIf config.services.dex.enable {
     extraConfig = lib.mkBefore (caddyHelpers.mkAllowedSources [ "136.199.45.0/24" ]);
   };
+  services.caddy.virtualHosts.hivegent = lib.mkIf config.custom.hivegent.enable {
+    extraConfig = lib.mkBefore (caddyHelpers.mkAllowedSources [ "136.199.45.0/24" ]);
+  };
 }
