9a6a4279a33659286125aa7267908024199c8eaa
Mirko Lenz <mirko@mirkolenz.com>
2026-05-06 14:53:41 +0200

    fix(users): remove unused quadlet user and group

--- nixos/users.nix
+++ nixos/users.nix
@@ -14,29 +14,9 @@ in
         group = admin.login;
         extraGroups = [ "wheel" ];
       };
-      quadlet = {
-        description = "Quadlet User";
-        isSystemUser = true;
-        uid = 502;
-        group = "quadlet";
-        linger = false;
-        subUidRanges = [
-          {
-            startUid = 200000;
-            count = 65536;
-          }
-        ];
-        subGidRanges = [
-          {
-            startGid = 300000;
-            count = 65536;
-          }
-        ];
-      };
     };
     groups = {
       ${admin.login}.gid = config.users.users.${admin.login}.uid;
-      quadlet.gid = config.users.users.quadlet.uid;
     };
   };
 }
