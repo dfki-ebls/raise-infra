42eda1db194352a9f7e8c3c6dc66e72b49bea18e
Mirko Lenz <mirko@mirkolenz.com>
2026-05-06 16:11:52 +0200

    fix(vpn): always inject allowed sources

--- dfki/vpn.nix
+++ dfki/vpn.nix
@@ -1,14 +1,13 @@
 {
-  config,
   lib,
   caddyHelpers,
   ...
 }:
 {
-  services.caddy.virtualHosts.rauthy = lib.mkIf config.custom.rauthy.enable {
+  services.caddy.virtualHosts.rauthy = {
     extraConfig = lib.mkBefore (caddyHelpers.mkAllowedSources [ "136.199.45.0/24" ]);
   };
-  services.caddy.virtualHosts.hivegent = lib.mkIf config.custom.hivegent.enable {
+  services.caddy.virtualHosts.hivegent = {
     extraConfig = lib.mkBefore (caddyHelpers.mkAllowedSources [ "136.199.45.0/24" ]);
   };
 }
